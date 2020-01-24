eab0ea5bfea9c06a82ef333e83f1b873bb50635c
dimansolo88 <dzmitry795@gmail.com>
2020-01-24 18:24:32 +0300

    added layout of head page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,61 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
+import {Layout, Menu, Breadcrumb, Switch} from 'antd';
+
+const {Header, Content} = Layout;
 
 function App() {
+    const [headerColor,setHeaderColor] = useState('dark')
+
+    const changeTheme = () => {
+        if(headerColor === 'dark') {
+            setHeaderColor('light')
+        }else {
+            setHeaderColor('dark')
+        }
+    }
+
     return (
         <div className="app">
-            <h1> online shop will be here </h1>
+            <Switch style={{position:"absolute", right:'20px'}}
+                checked={headerColor === 'dark'}
+                onChange={changeTheme}
+                checkedChildren="Dark"
+                unCheckedChildren="Light"
+            />
+            <Layout>
+
+                <Header className="header">
+                    <div className="logo"/>
+                    <Menu
+                        theme={headerColor}
+                        mode="horizontal"
+                        defaultSelectedKeys={['2']}
+                        style={{lineHeight: '64px'}}
+                    >
+                        <Menu.Item key="1">Home</Menu.Item>
+                        <Menu.Item key="2">About</Menu.Item>
+                    </Menu>
+                </Header>
+                <Layout>
+
+                    <Layout style={{padding: '0 24px 24px'}}>
+                        <Breadcrumb style={{margin: '16px 0'}}>
+
+                        </Breadcrumb>
+                        <Content
+                            style={{
+                                background: '#fff',
+                                padding: 24,
+                                margin: 0,
+                                minHeight: 280,
+                            }}
+                        >
+                            Content
+                        </Content>
+                    </Layout>
+                </Layout>
+            </Layout>,
         </div>
     );
 }
